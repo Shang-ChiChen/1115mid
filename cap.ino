97d011bf42cd6077c01875866f1a17cc11e2dd8e
Shang-ChiChen <83741986+Shang-ChiChen@users.noreply.github.com>
2021-11-15 21:46:31 +0800

    Create cap.ino

---
+++ cap.ino
@@ -0,0 +1,39 @@
+#include <Stepper.h> //引入步進馬達函示庫
+
+const int stepsPerRevolution = 2048;  //此馬達轉一圈為2048步
+const int RPM = 5; //馬達旋轉速度
+int incomingByte;
+
+//宣告Stepper物件，的步數和引腳
+//引角依序對應驅動版的1N1、1N3、1N2、1N4
+Stepper myStepper(stepsPerRevolution, 33, 14, 12, 27);  
+
+void setup() {  
+  Serial.begin(115200);
+  myStepper.setSpeed(RPM);  //設定馬達轉速
+  pinMode(26, INPUT);
+}
+int s = 0;
+int ps = 0; 
+void loop() {
+int input = analogRead(26);
+  Serial.println(input);
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+    Serial.print("I received: ");
+    Serial.println(incomingByte);
+      if (incomingByte == 49) {
+        Serial.println("Moving clockwise...");
+        myStepper.step(stepsPerRevolution/10);
+        delay(50);
+      }
+      else
+      if (incomingByte == 50) {
+        Serial.println("Moving counterclockwise...");
+        myStepper.step(-stepsPerRevolution/9);
+        delay(50);
+      }
+
+   }
+}
